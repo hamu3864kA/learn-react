0e4e02228f63b5497b752ddc0bbbfcff25cb4d19
hamu3864ka <cowkey.gem@gmail.com>
2022-09-15 14:11:45 +0000

    4-24 p177 プルダウンの内容をJSXに変更

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { useState } from "react";
 import "./styles.css";
 
 const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState('HTML');
+
+  const values = [
+    {id: 1, item: 'HTML'},
+    {id: 2, item: 'CSS'},
+    {id: 3, item: 'JavaScript'}
+  ];
+  
+  // これ自体はReactオブジェクト？ではない
+  // returnでReactオブジェクトを返す関数が関数コンポーネントになりうるのであって
+  // 帰ってきた結果自体はコンポーネントそのものではない
+  const SelectItems = values.map((value) => {
+    return (
+      <option key={value.id} value={value.item}>{value.item}</option>
+    );
+  });
+  
+  const [selectedValue, setSelectedValue] = useState(values[0].item);
   
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
   };
-  
+   
   return (
     <div className="App">
       <p>
@@ -15,9 +31,7 @@ const InputSelectBox = () => {
         <b>{selectedValue}</b>
       </p>
       <select value={selectedValue} onChange={handleChange}>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-        <option value="JavaScript">JavaScript</option>
+        {SelectItems}
       </select>
     </div>
   );
