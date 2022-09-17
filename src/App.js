8d162b321c3dd76531ce20a409fbd134b8d4a27a
hamu3864ka <cowkey.gem@gmail.com>
2022-09-17 05:32:18 +0000

    4-27 p190 オブジェクトで状態を管理するチェックボックス

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import React, { useState } from "react";
 import "./styles.css";
-const values = [
-  {id: 1, item: 'マウス'},
-  {id: 2, item: 'モニター'},
-  {id: 3, item: 'キーボード'},
-];
 
-// サブになる関数コンポーネント
-const CheckBtnItems = ({onChange, checked}) => {
-  return values.map((value) => {
-    return (
-      <label key={value.id}>
-        <input type="checkbox" value={value.item} onChange={onChange} checked={checked.includes(value.item)} />
-        {value.item}
-      </label>
-    );
-  });
-}
-
+// 複数のcheckedの状態をオブジェクトで管理する
 const InputCheckBox = () => {
-  const [checkedValues, setCheckedValues] = useState([]);
-  const handleChange = (event) => {
-    if (checkedValues.includes(event.target.value)) {
-      // 要するにチェックが外された場合
-      setCheckedValues(checkedValues.filter((checkedValue) => checkedValue !== event.target.value));
-    } else {
-      // チェックが付けられた場合
-      setCheckedValues([...checkedValues, event.target.value]);
-    }
+  const [checkedValues, setCheckedValues] = useState({
+    'マウス' : false,
+    'モニター' : false,
+    'キーボード' : false
+  });
 
-  }
+  const handleChange = (event) => {
+    // この書き方面白い
+    // これまでセッションに格納されたオブジェクトと今回操作されたチェックボックスをマージした新しい状態を作っている
+    setCheckedValues({...checkedValues, [event.target.value]: event.target.checked});
+  };
+
+  // 畳み込み演算をしている
+  // checkedValuesに依存しているから変更の都度再計算される？
+  const stateOfCheckedValues = Object.entries(checkedValues).reduce((pre, [key, value]) => {
+    // 値がtrueの場合、そのキーを追加する
+    // if (value) { pre.push(key); } と同じ
+    value && pre.push(key);
+    return pre;
+  }, [] /* 初期値は空の配列. */);
+
   return (
     <div className="App">
       <p>
-        現在選択されている値: <b>{checkedValues.join('、')}</b>
+        現在選択されている値: <b>{stateOfCheckedValues.join('、')}</b>
       </p>
-      <CheckBtnItems onChange={handleChange} checked={checkedValues} />
+      <label>
+        <input type="checkbox" value="マウス" onChange={handleChange} checked={checkedValues['マウス']} />
+        マウス
+      </label>
+      <label>
+        <input type="checkbox" value="モニター" onChange={handleChange} checked={checkedValues['モニター']} />
+        モニター
+      </label>
+      <label>
+        <input type="checkbox" value="キーボード" onChange={handleChange} checked={checkedValues['キーボード']} />
+        キーボード
+      </label>
     </div>
   );
 }
