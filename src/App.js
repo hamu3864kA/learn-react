f23860f1a400a939c02d19d08f56007273452271
hamu3864ka <cowkey.gem@gmail.com>
2022-09-20 13:47:19 +0000

    5-27 p215 useEffectのサンプル

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Counter from './section26/Counter';
 import Hello from './section26/Hello';
@@ -6,35 +6,30 @@ import Hello from './section26/Hello';
 export default function App() {
 
   const INITIAL_COUNT = 0;
-  const INITIAL_NAME = 'JavaScript';
 
-  const [count, setCount] = useState(INITIAL_COUNT);
-  const [name, setName] = useState(INITIAL_NAME);
+  const SampleComponent = () => {
+    const [count, setCount] = useState(INITIAL_COUNT);
 
-  // 状態更新関数にコールバックを渡した場合、コールバックの第一引数に現在の値が渡される
-  const countIncrement = () => setCount((prev) => prev + 1);
-  const countDecrement = () => setCount((prev) => prev - 1);
-  const countReset = () => setCount(INITIAL_COUNT);
+    const callbackFunction = () => {
+      document.title = `${count}回クリックされました`;
+    }
 
-  const handleChangeName = (e) => setName(e.target.value);
+    useEffect(callbackFunction, [count]);
+
+    const countIncrement = () => setCount((prevCount) => prevCount + 1);
+    const countReset = () => setCount(INITIAL_COUNT);
+
+    return (
+      <div className="App">
+        <p>現在のカウント数: {count}</p>
+        <button onClick={countIncrement}>+1ボタン</button>
+        <button onClick={countReset}>リセット</button>
+      </div>
+    );
+  };
 
   return (
-    <div className="App">
-      <p>ppp</p>
-      <Counter
-        count={count}
-        countIncrement={countIncrement}
-        countDecrement={countDecrement}
-        countReset={countReset}
-        initialCount={INITIAL_COUNT}
-      />
-      <Hello
-        name={name}
-        changeName={handleChangeName}
-        initialName={INITIAL_NAME}
-      />
-    </div>
-    
+    <SampleComponent />
   );
 
 }
---
+++ src/Section26App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Counter from './section26/Counter';
+import Hello from './section26/Hello';
+
+export default function App() {
+
+  const INITIAL_COUNT = 0;
+  const INITIAL_NAME = 'JavaScript';
+
+  const [count, setCount] = useState(INITIAL_COUNT);
+  const [name, setName] = useState(INITIAL_NAME);
+
+  // 状態更新関数にコールバックを渡した場合、コールバックの第一引数に現在の値が渡される
+  const countIncrement = () => setCount((prev) => prev + 1);
+  const countDecrement = () => setCount((prev) => prev - 1);
+  const countReset = () => setCount(INITIAL_COUNT);
+
+  const handleChangeName = (e) => setName(e.target.value);
+
+  return (
+    <div className="App">
+      <Counter
+        count={count}
+        countIncrement={countIncrement}
+        countDecrement={countDecrement}
+        countReset={countReset}
+        initialCount={INITIAL_COUNT}
+      />
+      <Hello
+        name={name}
+        changeName={handleChangeName}
+        initialName={INITIAL_NAME}
+      />
+    </div>
+    
+  );
+
+}
